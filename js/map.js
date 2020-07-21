a9a6292363cf3cfeb47fe5f9be95290f3b300d46
Burov Andrey <ananburov@gmail.com>
2020-07-21 15:06:35 +0300

    fix: fixed API map

--- js/map.js
+++ js/map.js
@@ -1,23 +1,37 @@
 ymaps.ready(init);
 
 function init() {
+  // Создание экземпляра карты и его привязка к контейнеру с
+  // заданным id ("map")
   var myMap = new ymaps.Map('map', {
-      center: [7.890703, 98.294772],
-      zoom: 15
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-    myPlacemark = new ymaps.Placemark(myMap.getCenter());
+    // При инициализации карты, обязательно нужно указать
+    // ее центр и коэффициент масштабирования
+    center: [7.890703, 98.294772], // Нижний Новгород
+    zoom: 15
+  });
 
+  // Создание метки 
+  var myPlacemark = new ymaps.Placemark(
+    // Координаты метки
+    [7.890703, 98.294772], {
+      // Свойства
+      // Текст метки
+      iconContent: 'Hilton Phuket Banthai Resort'
+    }, {
+      // Опции
+      // Иконка метки будет растягиваться под ее контент
+      preset: 'islands#redStretchyIcon',
+    });
+  // Добавление метки на карту
   myMap.geoObjects.add(myPlacemark);
 
   myPlacemark.events
     .add('mouseenter', function (e) {
       // Ссылку на объект, вызвавший событие,
       // можно получить из поля 'target'.
-      e.get('target').options.set('preset', 'islands#greenIcon');
+      e.get('target').options.set('preset', 'islands#redStretchyIcon');
     })
     .add('mouseleave', function (e) {
-      e.get('target').options.unset('preset');
+      e.get('target').options.unset('preset', 'islands#blackStretchyIcon');
     });
 }
