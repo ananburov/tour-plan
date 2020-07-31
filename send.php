236f82a39c78a52072fa98b62fb6c6a71f6d3fda
Burov Andrey <ananburov@gmail.com>
2020-07-31 12:54:50 +0300

    create: php send, modal forms + adaptiv form

---
+++ send.php
@@ -0,0 +1,65 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$newsletter = $_POST['newsletter'];
+
+// Формирование самого письма
+
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое письмо</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br><br>
+<b>Сообщение:</b><br>$message 
+";
+
+// Формирование самого письма
+$title2 = "Подписки на Best Tour Plan";
+$body2 = "
+<h2>Заявка на подписку на Best Tour Plan</h2>
+<b>Email:</b> $newsletter<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'burov.andrey.andreevich@gmail.com'; // Логин на почте
+    $mail->Password   = 'Y$XaPAzc'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('burov.andrey.andreevich@gmail.com', 'Андрей Буров'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('burov007@mail.ru');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
