ed927c011030af85b0784af67c2a7e02412a46fe
Burov Andrey <ananburov@gmail.com>
2020-08-03 19:24:51 +0300

    add: added Esc close modal window

--- js/main.js
+++ js/main.js
@@ -50,6 +50,15 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+  $(document).keyup(function(e){
+if(e.keyCode == 27) {}
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  });
+
   //Обработка форм
   $(".form").each(function(){
     $(this).validate({
