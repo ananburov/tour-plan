4cebaf3a5e82a86c75de5442c8100ae6692882d0
Burov Andrey <ananburov@gmail.com>
2020-08-03 17:48:28 +0300

    create: validate forms, size small-card__button

--- js/main.js
+++ js/main.js
@@ -50,4 +50,46 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  //Обработка форм
+  $(".form").each(function(){
+    $(this).validate({
+      errorClass: "invalid",
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        }
+      },
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: jQuery.validator.format("At least {0} characters required!"),
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please enter your phone number",
+        },
+      },
+    });
+  });
+
+
+  $(".subscribe").each(function () {
+    $(this).validate({
+      errorClass: "invalid-email",
+        newsletter: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    );
+  });
+
+  $(document).ready(function () {
+    $('#phone').mask('+7 (999) 999-99-99');
+    $('phone').mask('+7 (999) 999-99-99');
+  });
 });
